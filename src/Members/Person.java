3f22570c23fbbf5219c57a77fb4d19ff7df15b0e
Nikhil Karmakar <nikhilkarmakar4020@gmail.com>
2024-11-25 23:59:08 +0530

    intial commit

---
+++ src/Members/Person.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class Person {
+    private String name;
+    private LocalDate dob;
+    private String contact;
+
+    public Person(String name, LocalDate dob, String contact) {
+        this.name = name;
+        this.dob = dob;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public int getAge() {
+        return Period.between(dob, LocalDate.now()).getYears();
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", DOB: " + dob + ", Contact: " + (contact != null ? contact : "N/A");
+    }
+
+    public abstract void role(); // Force subclasses to define their role
+}
---
+++ src/Members/Student.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+// import java.time.Period;
+
+public class Student extends Person {
+    private String Student_Id;
+    private String Standard;
+
+    Student(String name, LocalDate dob, String contact, String Student_Id, String Standard) {
+        super(name, dob, contact);
+        this.Standard = Standard;
+        this.Student_Id = Student_Id;
+    }
+
+    @Override
+    public void role() {
+        System.out.println("I am a student with ID: " + Student_Id);
+    }
+
+    public String getStandard() {
+        return Standard;
+    }
+
+    public void setStandard(String STD) {
+        Standard = STD;
+    }
+
+    public String getStudentID() {
+        return Student_Id;
+    }
+
+    public void setStudentID(String ID) {
+        Student_Id = ID;
+    }
+
+}
